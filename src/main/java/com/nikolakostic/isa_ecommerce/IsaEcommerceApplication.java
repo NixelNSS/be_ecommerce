5c7e94d1dd205249e2718884d51e90a76a8f02d1
Nikola Kostic <nixel@pop-os.localdomain>
2021-05-27 11:14:19 +0200

    Initial commit.

---
+++ src/main/java/com/nikolakostic/isa_ecommerce/IsaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.nikolakostic.isa_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsaEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikolakostic/isa_ecommerce/IsaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolakostic.isa_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
