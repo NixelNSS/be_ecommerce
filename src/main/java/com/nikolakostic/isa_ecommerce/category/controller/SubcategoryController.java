24b394e7f439c01da97deee69c5521946a8f02ac
Nikola Kostic <nixel@pop-os.localdomain>
2021-06-09 22:35:22 +0200

    Add subcategory getAll method in controller.

--- src/main/java/com/nikolakostic/isa_ecommerce/category/controller/SubcategoryController.java
+++ src/main/java/com/nikolakostic/isa_ecommerce/category/controller/SubcategoryController.java
@@ -2,7 +2,10 @@ package com.nikolakostic.isa_ecommerce.category.controller;
 
 import com.nikolakostic.isa_ecommerce.category.service.SubcategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +16,14 @@ public class SubcategoryController {
 
     @Autowired
     private SubcategoryService subcategoryService;
+
+    @GetMapping("all")
+    public ResponseEntity<?> getAll() {
+        try {
+            return ResponseEntity.ok().body(this.subcategoryService.getAll());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
 }
--- src/main/java/com/nikolakostic/isa_ecommerce/category/service/SubcategoryService.java
+++ src/main/java/com/nikolakostic/isa_ecommerce/category/service/SubcategoryService.java
@@ -1,7 +1,20 @@
 package com.nikolakostic.isa_ecommerce.category.service;
 
+import com.nikolakostic.isa_ecommerce.category.entity.Subcategory;
+import com.nikolakostic.isa_ecommerce.category.repository.SubcategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SubcategoryService {
+
+    @Autowired
+    SubcategoryRepository subcategoryRepository;
+
+    public List<Subcategory> getAll() {
+        return this.subcategoryRepository.findAll();
+    }
+
 }
